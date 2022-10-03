f76351dc3ccd820beef16130c02d19d3819defd2
abumahmud <alabiabdulbaasit@gmail.com>
2022-10-03 00:08:20 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - program that prints its name, followed by a new line
+ * main - program that prints its name, followed by a newline
  * @argc: argument that contains argument input
- * @argv: argument that stores the strings in an array of char* (strings)
+ * @argv: argument that scores the strings in an array of char* (strings)
  * Return: 0
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char * argv[])
 {
 	int num_1, num_2, mul;
 
-	if (arg != 3)
+	if (argc != 3)
 		printf("Error\n");
 	else
 	{
