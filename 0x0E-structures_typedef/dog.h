715c981140d04ac3d88d3725bb55d2484d24c3cd
abumahmud <alabiabdulbaasit@gmail.com>
2022-10-10 20:58:33 -0700

    prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,13 @@
-Structures
-
-Start with dog.h
-
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - A new type describing a dog.
+ * struct dog - A type describing a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
+
 struct dog
 {
 	char *name;
